9c47ca16ad2ac6c148c528bc04e625579c120fd4
DESKTOP-B949T0J\jangho <jangho309@naver.com>
2024-07-13 13:23:18 +0900

    24.07.13 CSS, JS, Html, images 파일 경로 수정 및 일부 파일 기능 오류 수정, controller 카테고리별 생성

---
+++ controller/CommunityController.java
@@ -0,0 +1,22 @@
+package com.test.api.controller;
+
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class CommunityController {
+
+    private Logger logger = (Logger) LoggerFactory.getLogger(CommunityController.class);
+
+    @GetMapping("/community")
+    public ModelAndView community() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("community/community");
+
+        logger.debug("community페이지 이동");
+        return mav;
+    }
+}
---
+++ controller/RandomController.java
@@ -0,0 +1,22 @@
+package com.test.api.controller;
+
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class RandomController {
+
+    private Logger logger = (Logger) LoggerFactory.getLogger(RandomController.class);
+
+    @GetMapping("/randomPin")
+    public ModelAndView randomPin() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("randomPin/randomPin");
+
+        logger.debug("randomPin페이지 이동");
+        return mav;
+    }
+}
