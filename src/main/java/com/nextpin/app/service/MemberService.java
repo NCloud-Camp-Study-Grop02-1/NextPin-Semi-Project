e99742af99e106fb32605d7eb3ce4b3401301b52
vktsi <havebeen4@naver.com>
2024-08-02 10:31:00 +0900

    24.08.02 오유빈 회원가입 백엔드 제작 중간 업로드

---
+++ src/main/java/com/nextpin/app/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.nextpin.app.service;
+
+import com.nextpin.app.dto.MemberDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface MemberService {
+    void signUp(MemberDto memberDto);
+
+    List<MemberDto> getMembers();
+
+    MemberDto getMemberByUserId(MemberDto memberDto);
+
+    String userIdCheck(String userId);
+
+    String userNicknameCheck(String nickname);
+
+    MemberDto login(MemberDto memberDto);
+}
